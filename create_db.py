a8f6ba237d28934e6f4bd4ab20f716bf88e48f0b
Thrawn1 <b.eremin@gmail.com>
2023-05-15 11:27:05 +0300

    Update create_db.py

--- create_db.py
+++ create_db.py
@@ -22,36 +22,30 @@ def create_db():
         middle_name TEXT,
         family TEXT,
         age INTEGER,
-        id_subdivision INTEGER,
-        mobile_phone INTEGER
-        FOREIGN KEY (id_subdivision) REFERENCES subdivision(id_subdivision)
+        id_subdivision INTEGER REFERENCES subdivision(id_subdivision),
+        mobile_phone INTEGER 
         )""")
     cursor.execute("""CREATE TABLE IF NOT EXISTS subdivision(
         id_subdivision INTEGER PRIMARY KEY AUTOINCREMENT,
         name_subdivision TEXT
-        FOREIGN KEY (id_subdivision) REFERENCES employees(id_subdivision)
         )""")
     cursor.execute("""CREATE TABLE IF NOT EXISTS status(
         id_status INTEGER PRIMARY KEY AUTOINCREMENT,
         name_status TEXT
-        FOREIGN KEY (id_status) REFERENCES request_support(status)
         )""")
     cursor.execute("""CREATE TABLE IF NOT EXISTS subdivision_support(
         id_support INTEGER PRIMARY KEY AUTOINCREMENT,
         name_support TEXT,
-        id_supervisor INTEGER
-        FOREIGN KEY (id_supervisor) REFERENCES employees(id)
+        id_supervisor INTEGER REFERENCES employees(id)
         )""")
     cursor.execute("""CREATE TABLE IF NOT EXISTS request_support(
         id_request INTEGER PRIMARY KEY AUTOINCREMENT,
-        id_employee INTEGER,
-        id_subdivision_support INTEGER,
-        date_request TEXT,
-        status INTEGER,
-        date_support TEXT
-        FOREIGN KEY (id_employee) REFERENCES employees(id)
-        FOREIGN KEY (id_subdivision_support) REFERENCES subdivision_support(id_support)
-        FOREIGN KEY (status) REFERENCES status(id_status)
+        id_employee INTEGER REFERENCES employees(id),
+        id_subdivision_support INTEGER REFERENCES subdivision_support(id_support),
+        description TEXT,
+        date_request DATE,
+        status INTEGER REFERENCES status(id_status),
+        date_support DATE 
         )""")
 
     conn.commit()
