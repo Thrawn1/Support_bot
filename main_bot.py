d98442019b8d5ba8239c6ac3348b89e03f4c9a52
Thrawn1 <b.eremin@gmail.com>
2023-06-12 22:38:23 +0300

    Update main_bot.py

--- main_bot.py
+++ main_bot.py
@@ -3,14 +3,15 @@ from aiogram.dispatcher.filters import Text as text
 from aiogram.contrib.fsm_storage.memory import MemoryStorage
 from aiogram.dispatcher.filters.state import State, StatesGroup
 from aiogram.dispatcher import FSMContext
+from aiogram.types import ReplyKeyboardRemove, ReplyKeyboardMarkup, KeyboardButton, InlineKeyboardMarkup, InlineKeyboardButton
 import logging
 from config import BOT_TOCKEN
 
 
 storage = MemoryStorage()
 bot = Bot(BOT_TOCKEN)
-dp = Dispatcher(bot)
-logging.basicConfig(level=logging.DEBUG, filename='bot.log', filemode='w')
+dp = Dispatcher(bot=bot, storage=storage)
+logging.basicConfig(level=logging.INFO, filename='bot.log', filemode='w')
 
 
 class ClientStates(StatesGroup):
@@ -22,176 +23,168 @@ class ClientStates(StatesGroup):
     printer = State()
     scaner = State()
     computer = State()
+    choice_main = State()
+    choice_problem = State()
     
     
-    
-
-
-def create_request(flag, message: types.Message):
-    print(flag.get())
-    if flag.get() != '0':
-        if flag == 'scaner':
-            message.answer("Заглушка. Создание заявки на сканер")
-        elif flag == 'computer':
-            message.answer("Заглушка. Создание заявки на компьютер")
-        elif flag == 'mfu':
-            message.answer("Заглушка. Создание заявки на МФУ")
-        elif flag == 'software':
-            message.answer("Заглушка. Создание заявки на программное обеспечение")
-        elif flag == 'web':
-            message.answer("Заглушка. Создание заявки на доступ в интернет")
-        elif flag == 'license':
-            message.answer("Заглушка. Создание заявки на лицензии")
-        elif flag == 'manual':
-            message.answer("Заглушка. Создание заявки в IT отдел")
-        elif flag == 'printer':
-            message.answer("Заглушка. Создание заявки на принтер")
-            print(flag) 
-        else:
-            print("Error")
-    else:
-        pass
-
-
-
-@dp.message_handler(commands=['start'])
-async def process_start_command(message: types.Message):
-    await message.reply("Здравствуйте!\nВас привествует бот технической поддержки")
+def keyboard_main_menu() -> ReplyKeyboardMarkup:
     keyboard = types.ReplyKeyboardMarkup(resize_keyboard=True)
     buttons = ["Поиск решения проблемы", "Заявка в IT отдел"]
     keyboard.add(*buttons)
-    await message.answer("Выберете режим работы", reply_markup=keyboard)
+    return keyboard
+
+def keyboard_yes_no() -> ReplyKeyboardMarkup:
+    keyboard = types.ReplyKeyboardMarkup(resize_keyboard=True)
+    buttons = ["Да", "Нет"]
+    keyboard.add(*buttons)
+    return keyboard
+
+def keyboard_problem() -> ReplyKeyboardMarkup:
+    keyboard = types.ReplyKeyboardMarkup(resize_keyboard=True)
+    buttons = ["Не работоспобность обрудования", "Проблемы с программным обеспечением", "Проблемы с доступом в интернет", "Проблемы с лицензиями"]
+    keyboard.add(*buttons)
+    return keyboard
+
+def keyboard_equipment() -> ReplyKeyboardMarkup:
+    keyboard = types.ReplyKeyboardMarkup(resize_keyboard=True)
+    buttons = ["Принтер", "Сканер", "Компьютер", "МФУ"]
+    keyboard.add(*buttons)
+    return keyboard
+
+@dp.message_handler(commands=['start'])
+async def process_start_command(message: types.Message):
+    await message.reply("Здравствуйте!\nВас привествует бот технической поддержки",reply_markup=keyboard_main_menu())
+    await message.answer("Выберете режим работы")
+    await ClientStates.choice_main.set()
 
 
 @dp.message_handler(text(equals="Нет"))
-async def process_start_command(message: types.Message,flag):
-    path = 'help_resourse/'
-    file_name = 'request_manual'
-    print(path + file_name)
-    with open((path + file_name), 'r',encoding='utf-8') as file:
-        text = file.read()
-    await message.answer(text)
-    flag_1 = flag.get()
-    create_request(flag_1, message)
-
+async def slect_no(message: types.Message):
+    await ClientStates.manual.set()
+    await create_manual_request(message)
+    
 
 
 @dp.message_handler(text(equals="Да"))
-async def process_start_command(message: types.Message):
+async def process_end(message: types.Message, state: FSMContext):
     path = 'help_resourse/'
     file_name = 'thanks'
     print(path + file_name)
     with open((path + file_name), 'r',encoding='utf-8') as file:
         text = file.read()
     await message.answer(text)
-    flag = '0'
+    await state.finish()
 
 
 
 @dp.message_handler(text(equals="Заявка в IT отдел"))
-async def process_start_command(message: types.Message):
-    await message.answer("Заглушка. Создание заявки")
-    flag = 'manual'
-
-
-
-
-
-
-@dp.message_handler(text(equals="Поиск решения проблемы"))
-async def process_start_command(message: types.Message):
-    #await message.answer("Когда нибудь Миша напишет диплом")
+async def create_manual_request(message: types.Message):
+    path = 'help_resourse/'
+    file_name = 'request_manual'
+    print(path + file_name)
+    with open((path + file_name), 'r',encoding='utf-8') as file:
+        text = file.read()
+    await message.answer(text)
     keyboard = types.ReplyKeyboardMarkup(resize_keyboard=True)
-    buttons = ["Не работоспобность обрудования", "Проблемы с программным обеспечением", "Проблемы с доступом в интернет", "Проблемы с лицензиями"]
-    keyboard.add(*buttons)  
-    await message.answer("Выберете категорию проблемы", reply_markup=keyboard)
+    buttons = ["Отмена"]
+    keyboard.add(*buttons)
+
+
+@dp.message_handler(text(equals="Отмена"))
+async def process_cancel(message: types.Message, state: FSMContext):
+    path = 'help_resourse/'
+    file_name = 'thanks'
+    print(path + file_name)
+    with open((path + file_name), 'r',encoding='utf-8') as file:
+        text = file.read()
+    await message.answer(text)
+    await state.finish()
+
+
+@dp.message_handler(text(equals="Поиск решения проблемы"), state = ClientStates.choice_main)
+async def process_select_problem(message: types.Message, state: FSMContext):
+    #await message.answer("Когда нибудь Миша напишет диплом")
+    await message.answer("Выберете категорию проблемы", reply_markup=keyboard_problem())
+    await ClientStates.next()
 
 
 @dp.message_handler(text(equals="Не работоспобность обрудования"))
-async def process_start_command(message: types.Message):
-    await message.answer("Заглушка. Не работоспобность обрудования")
-    keyboard = types.ReplyKeyboardMarkup(resize_keyboard=True)
-    buttons = ["Принтер", "Компьютер", "Сканер", "МФУ"]
-    keyboard.add(*buttons)  
-    await message.answer("Выберете подкатегорию проблемы", reply_markup=keyboard)
+async def process_select_equipment(message: types.Message):
+    await message.answer("Выберете подкатегорию проблемы", reply_markup=keyboard_equipment())
 
 
 @dp.message_handler(text(equals="Проблемы с программным обеспечением"))
-async def process_start_command(message: types.Message):
+async def software_problem(message: types.Message):
     path = 'help_resourse/'
     file_name = 'software'
     print(path + file_name)
     with open((path + file_name), 'r',encoding='utf-8') as file:
         text = file.read()
     await message.answer(text)
-    await message.answer("Данная информация помогла вам?", reply_markup=types.ReplyKeyboardMarkup(resize_keyboard=True).add("Да", "Нет"))
-    flag = 'software'
+    await message.answer("Данная информация помогла вам?", reply_markup=keyboard_yes_no())
+
 
 @dp.message_handler(text(equals="Проблемы с доступом в интернет"))
-async def process_start_command(message: types.Message):
+async def web_problem(message: types.Message):
     path = 'help_resourse/'
     file_name = 'web'
     print(path + file_name)
     with open((path + file_name), 'r',encoding='utf-8') as file:
         text = file.read()
     await message.answer(text)
-    await message.answer("Данная информация помогла вам?", reply_markup=types.ReplyKeyboardMarkup(resize_keyboard=True).add("Да", "Нет"))
-    flag = 'web'
+    await message.answer("Данная информация помогла вам?", reply_markup=keyboard_yes_no())
+
 
 @dp.message_handler(text(equals="Проблемы с лицензиями"))
-async def process_start_command(message: types.Message):
+async def license_problem(message: types.Message):
     path = 'help_resourse/'
     file_name = 'license'
     print(path + file_name)
     with open((path + file_name), 'r',encoding='utf-8') as file:
         text = file.read()
     await message.answer(text)
-    await message.answer("Данная информация помогла вам?", reply_markup=types.ReplyKeyboardMarkup(resize_keyboard=True).add("Да", "Нет"))
-    flag = 'license'
+    await message.answer("Данная информация помогла вам?", reply_markup=keyboard_yes_no())
 
 
 @dp.message_handler(text(equals="Принтер"))
-async def process_start_command(message: types.Message):
+async def printer_problem(message: types.Message):
     path = 'help_resourse/'
     file_name = 'printer'
     print(path + file_name)
     with open((path + file_name), 'r',encoding='utf-8') as file:
         text = file.read()
     await message.answer(text)
-    await message.answer("Данная информация помогла вам?", reply_markup=types.ReplyKeyboardMarkup(resize_keyboard=True).add("Да", "Нет"))
+    await message.answer("Данная информация помогла вам?", reply_markup=keyboard_yes_no())
 
 @dp.message_handler(text(equals="Сканер"))
-async def process_start_command(message: types.Message):
+async def scaner_problem(message: types.Message):
     path = 'help_resourse/'
     file_name = 'scaner'
     print(path + file_name)
     with open((path + file_name), 'r',encoding='utf-8') as file:
         text = file.read()
     await message.answer(text)
-    await message.answer("Данная информация помогла вам?", reply_markup=types.ReplyKeyboardMarkup(resize_keyboard=True).add("Да", "Нет"))
-    flag = 'scaner'
+    await message.answer("Данная информация помогла вам?", reply_markup=keyboard_yes_no())
 
 @dp.message_handler(text(equals="Компьютер"))
-async def process_start_command(message: types.Message):
+async def computer_problem(message: types.Message):
     path = 'help_resourse/'
     file_name = 'computer'
     print(path + file_name)
     with open((path + file_name), 'r',encoding='utf-8') as file:
         text = file.read()
     await message.answer(text)
-    await message.answer("Данная информация помогла вам?", reply_markup=types.ReplyKeyboardMarkup(resize_keyboard=True).add("Да", "Нет"))
-    flag = 'computer'
+    await message.answer("Данная информация помогла вам?", reply_markup=keyboard_yes_no())
 
 @dp.message_handler(text(equals="МФУ"))
-async def process_start_command(message: types.Message):
+async def mfu_problem(message: types.Message):
     path = 'help_resourse/'
     file_name = 'mfu'
     print(path + file_name)
     with open((path + file_name), 'r',encoding='utf-8') as file:
         text = file.read()
     await message.answer(text)
-    await message.answer("Данная информация помогла вам?", reply_markup=types.ReplyKeyboardMarkup(resize_keyboard=True).add("Да", "Нет"))
-    flag = 'mfu'
+    await message.answer("Данная информация помогла вам?", reply_markup=keyboard_yes_no())
 
 
 if __name__ == '__main__':
